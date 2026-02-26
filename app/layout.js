ca1897c032717cf03a31088685dd7e9ba614cd87
vikashjiop89 <vikashjiop89@gmail.com>
2026-02-26 08:47:08 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,14 @@
 export const metadata = {
-  title: "Chatee",
-  description: "Landing page",
+  title: "Chatee - Private Video Calling App",
+  description: "Secure & Private Video Calling Application",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
     </html>
   );
 }
